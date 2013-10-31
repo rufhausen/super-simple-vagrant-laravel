0e48183f8243764840b2efb8a9290827c80fb0f9
Gary Taylor <gtaylor@electricwerks.com>
2013-10-31 10:37:12 -0600

    added stuff for mysql root pass, 1GB of memory, OAuth, GD, xdebug

--- Vagrantfile
+++ Vagrantfile
@@ -4,11 +4,15 @@
 Vagrant.configure("2") do |config|
 
   config.vm.box = "precise32"
-  config.vm.hostname = "videocast-local"
-  config.vm.network "private_network", ip: "192.168.50.3"
+  config.vm.hostname = "test-local"
+  config.vm.network "private_network", ip: "192.168.50.4"
   config.vm.provision :shell, :path => "bootstrap.sh"
-  #apache
-  config.vm.network :forwarded_port, host: 9012, guest: 80
+  config.vm.provider :virtualbox do |vb|
+          vb.customize [ "modifyvm", :id, "--memory", 1024]
+  end
+  #apache/mysql
+  config.vm.network :forwarded_port, host: 9013, guest: 80
+  config.vm.network :forwarded_port, host: 3307, guest: 3306
   #file permissions
   config.vm.synced_folder ".", "/vagrant", :mount_options => ["dmode=777","fmode=666"]
 
--- bootstrap.sh
+++ bootstrap.sh
@@ -10,22 +10,32 @@
 
 apt-get update
 apt-get install -y apache2
-apt-get install php5
+apt-get install -y php5
 apt-get install -y libapache2-mod-php5
 apt-get install -y php5-cli
 apt-get install -y php5-mysql
 apt-get install -y php5-curl
 apt-get install -y php5-mcrypt
-apt-get install php5-dev
-sudo apt-get install php-apc
-apt-get install make
+apt-get install -y php5-dev
+apt-get install -y php5-xdebug
+apt-get install -y  php-apc
+apt-get install -y php5-gd
+apt-get install -y php-pear
+apt-get install -y libcurl3-openssl-dev
+pecl install -y pecl_http
+pecl install -y oauth
+apt-get install -y libpcre3-dev
+apt-get install -y make
 apt-get install -y curl
 curl -s https://getcomposer.org/installer | php
 mv composer.phar /usr/local/bin/composer
 export DEBIAN_FRONTEND=noninteractive
+debconf-set-selections <<< 'mysql-server-5.5 mysql-server/root_password password root'
+debconf-set-selections <<< 'mysql-server-5.5 mysql-server/root_password_again password root'
 apt-get -q -y install mysql-server-5.5
 apt-get install -y git
-apt-get install zsh
+apt-get install -y zsh
+
 
 #END FIRST RUN STUFF
 
